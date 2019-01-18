24d2d237bda2a947428f4f96abec6f84197b1a8c
ericanvoued <ericanvoued@gmail.com>
2019-01-19 00:00:52 +0800

    ...

--- assest/data/index.js
+++ assest/data/index.js
@@ -1,162 +1,162 @@
 export const list = {
   "data": [{
     "imgUrl": "../../assest/icon/1.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   }, {
     "imgUrl": "../../assest/icon/2.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/3.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/4.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/5.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/6.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/7.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/8.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/9.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/10.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/11.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/12.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/13.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/14.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/15.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/16.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/17.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/18.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/19.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/20.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/21.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/22.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/23.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/24.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/25.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/26.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/27.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/28.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/29.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/30.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/31.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   },
   {
     "imgUrl": "../../assest/icon/32.png",
-    "text": "其余微信群已经加满，此处位置实时更新微信号:yabo2888",
-    "title": "亚博原生app"
+    "text": "其余微信号已经加满，此处位置实时更新微信号:chen_qiang_",
+    "title": "陈强的小程序"
   }
   ]
 }
